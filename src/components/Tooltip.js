fbac081078ad328910240d29d32e155ad218169a
nikrb <nik_rb@yahoo.com>
2017-06-27 11:44:42 +0100

    add bar chart and tooltip components

---
+++ src/components/Tooltip.js
@@ -0,0 +1,28 @@
+/**
+ * usage:
+  <ToolTip tip_text={tooltip_text} pos={tooltip} />
+  where pos is: {
+    display: ["block"|"none"],
+    top: tooltip_y,
+    left: tooltip_x
+  }
+ */
+import React from 'react';
+
+export default ( props) => {
+  const style =  {
+    position: "absolute",
+    textAlign: "center"
+    padding: "5px",
+    font: "12px sans-serif",
+    background: "lightsteelblue",
+    border: "0px",
+    borderRadius: "8px",
+    pointerEvents: "none"
+  };
+  return (
+    <div style={{...style, props.pos}}>
+      {props.tip_text}
+    </div>
+  );
+};
