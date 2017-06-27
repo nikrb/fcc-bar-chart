4b3b16e37ca98ef9c6edbbf0d836836475176e32
nikrb <nik_rb@yahoo.com>
2017-06-27 14:39:06 +0100

    use GDP component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GDP from './components/GDP';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <GDP />
       </div>
     );
   }
